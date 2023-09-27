a16c53d0516d31b8a2d039f97b7b7d77b004de7c
bpiroman <b.piromanski@holdfasteng.com>
2023-09-28 03:27:32 +0800

    added refresh function

--- app.js
+++ app.js
@@ -13,22 +13,48 @@
 //     })
 // });
 
-$('#getJson').on('click', function(){
+const wowApp = function(data) {
+    console.log(data);
+    var newContent = '';
+    var title = data[0].movie;
+    var date = data[0].year;
+    var video = data[0].video['720p'];
+    var img = data[0].poster;
+    newContent += `<h1>${title} (${date})</h1>`
+    newContent += `<video controls width="500"><source src="${video}" type="video/mp4" /></video>`;
+    newContent += `<br>`;
+    newContent += `<img src="${img}" class="contentImg"></img>`;
+    $('#wow').append(newContent);
+};
+
+$(document).ready(function() {
+    // Handler for .ready() called.
+    $.getJSON('https://owen-wilson-wow-api.onrender.com/wows/random')
+    .done(function(data){
+        wowApp(data);
+        // console.log(data);
+        // var newContent = '';
+        // var title = data[0].movie;
+        // var date = data[0].year;
+        // var video = data[0].video['720p'];
+        // var img = data[0].poster;
+        // newContent += `<h1>${title} (${date})</h1>`
+        // newContent += `<video controls width="500"><source src="${video}" type="video/mp4" /></video>`;
+        // newContent += `<br>`;
+        // newContent += `<img src="${img}" class="contentImg"></img>`;
+        // $('#wow').append(newContent);
+    }).fail(function(){
+        console.log('something failed');
+    }).always(function(){
+        console.log('always works... baby');
+    })
+});
+
+$('#getJson').on('ready click', function(){
     $('#wow').empty();
     $.getJSON('https://owen-wilson-wow-api.onrender.com/wows/random')
     .done(function(data){
-        console.log(data);
-        var newContent = '';
-        var title = data[0].movie;
-        var date = data[0].year;
-        var video = data[0].video['720p'];
-        var img = data[0].poster;
-        // var video = data[0].
-        newContent += `<h1>${title} (${date})</h1>`
-        newContent += `<video controls width="500"><source src="${video}" type="video/mp4" /></video>`;
-        newContent += `<br>`;
-        newContent += `<img src="${img}" class="contentImg"></img>`;
-        $('#wow').append(newContent);
+        wowApp(data);
     }).fail(function(){
         console.log('something failed');
     }).always(function(){
